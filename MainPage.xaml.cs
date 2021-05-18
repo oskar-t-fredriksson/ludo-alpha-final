3bfc2ac8a091c09d3511c369df9b74ee4a445fbe
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2021-05-18 09:46:24 +0200

    Added basic variables and methods for GameCanvas

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Graphics.Canvas.UI;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,19 +27,37 @@ namespace Ludo_alpha_final
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static CanvasBitmap MenuScreen;
+        public static Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+        public static float DesignWidth = 1280;
+        public static float DesignHeight = 720;
+        public static float scaleWidth, scaleHeight;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Window.Current.SizeChanged += Current_SizeChanged;
+            
+        }
+
+        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void GameCanvas_CreateResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
         {
+            args.TrackAsyncAction(CreateResourceAsync(sender).AsAsyncAction());
+        }
 
+        async Task CreateResourceAsync(CanvasControl sender)
+        {
+            MenuScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/BG.png"));
         }
 
         private void GameCanvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-
+            args.DrawingSession.DrawImage(Scaling.Img(MenuScreen));
         }
 
         private void GameCanvas_Tapped(object sender, TappedRoutedEventArgs e)
---
+++ Scaling.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo_alpha_final
+{
+    class Scaling
+    {
+        public static void SetScale()
+        {
+            //Display information            
+            MainPage.scaleWidth = (float)MainPage.bounds.Width / MainPage.DesignWidth;
+            MainPage.scaleHeight = (float)MainPage.bounds.Height / MainPage.DesignHeight;
+        }
+
+        public static Transform2DEffect Img(CanvasBitmap source)
+        {
+            Transform2DEffect image;
+            image = new Transform2DEffect() { Source = source };
+            image.TransformMatrix = Matrix3x2.CreateScale(MainPage.scaleWidth, MainPage.scaleHeight);
+            return image;
+        }
+    }
+}
